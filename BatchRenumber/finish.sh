10bc9425803fbb155b3b95100234272be3977d34
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-12-08 19:08:02 -0800

    modified finish to not move to /usr/local/bin/custom

--- BatchRenumber/finish.sh
+++ BatchRenumber/finish.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/bash
 
-
 javac Renumber.java
 Sleep 2s
 echo ""
-jar -cvfm Renumber.jar MANIFEST.mf *.class
+
+# Building the Jar file
+jar -cvfm Renumber.jar MANIFEST.mf ./*.class
 echo ""
 echo "Compiled Renumber.jar"
 
 sleep 2s
 
-cp Renumber.jar /usr/local/bin/java
-cp br_config /usr/local/bin/java
+if [ ! \( -d br_config \) ];
+  then
+    mkdir br_config && cd br_config || return
+    touch filter.txt init.txt && cd ..
+fi
 
-echo "Moved Renumber to /usr/local/bin/java"
+echo "Created the Jar and config files"
